bedce17808184178717ccf56b7ccb5c1d72463df
Davit Vardanyan <davo81@gmail.com>
2023-12-28 14:25:24 +0400

    Add Lists Support

---
+++ src/Factories/ListFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Label84\ActiveCampaign\Factories;
+
+use Label84\ActiveCampaign\DataObjects\ActiveCampaignList;
+
+class ListFactory
+{
+    /**
+     * @param array<string> $attributes
+     */
+    public static function make(array $attributes): ActiveCampaignList
+    {
+        return new ActiveCampaignList(
+            $attributes['id'],
+            $attributes['name'],
+            $attributes['stringid'],
+            $attributes['sender_url'],
+            $attributes['sender_reminder'],
+            $attributes['send_last_broadcast'],
+            $attributes['carboncopy'],
+            $attributes['subscription_notify'],
+            $attributes['unsubscription_notify'],
+            $attributes['user'],
+        );
+    }
+}
---
+++ src/Resources/ActiveCampaignListResource.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Label84\ActiveCampaign\Resources;
+
+use Illuminate\Support\Collection;
+use Label84\ActiveCampaign\DataObjects\ActiveCampaignContact;
+use Label84\ActiveCampaign\DataObjects\ActiveCampaignList;
+use Label84\ActiveCampaign\Exceptions\ActiveCampaignException;
+use Label84\ActiveCampaign\Factories\ContactFactory;
+
+class ActiveCampaignListResource extends ActiveCampaignBaseResource
+{
+    /**
+     * Retrieve an existing list by its ID.
+     *
+     * @see https://developers.activecampaign.com/reference/retrieve-a-list
+     *
+     * @throws ActiveCampaignException
+     */
+    public function get(string $id): ActiveCampaignContact
+    {
+        return ContactFactory::make($this->request(
+            method: 'get',
+            path: 'lists/'.$id,
+            responseKey: 'list'
+        ));
+    }
+
+    /**
+     * List all lists, search lists, or filter lists by query defined criteria.
+     *
+     * @see https://developers.activecampaign.com/reference/retrieve-all-lists
+     *
+     * @return Collection<int, ActiveCampaignList>
+     *
+     * @throws ActiveCampaignException
+     */
+    public function list(?string $query = ''): Collection
+    {
+        $lists = $this->request(
+            method: 'get',
+            path: 'contacts?'.$query,
+            responseKey: 'contacts'
+        );
+
+        return collect($lists)
+            ->map(fn ($list) => ContactFactory::make($list));
+    }
+
+    /**
+     * Create a list and return the list ID.
+     *
+     * @see https://developers.activecampaign.com/reference/create-new-list
+     *
+     * @throws ActiveCampaignException
+     */
+    public function create(string $email, array $attributes = []): string
+    {
+        $list = $this->request(
+            method: 'post',
+            path: 'lists',
+            data: [
+                'list' => $attributes,
+            ],
+            responseKey: 'list'
+        );
+
+        return $list['id'];
+    }
+
+    /**
+     * Delete an existing list by its ID.
+     *
+     * @see https://developers.activecampaign.com/reference/delete-a-list
+     *
+     * @throws ActiveCampaignException
+     */
+    public function delete(string $id): void
+    {
+        $this->request(
+            method: 'delete',
+            path: 'lists/'.$id
+        );
+    }
+}
